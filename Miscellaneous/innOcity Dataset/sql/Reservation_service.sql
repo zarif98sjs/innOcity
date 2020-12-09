282545e9863d50f3a68ce17418b776deb99660a4
Ramisa Alam <44811300+ramisa2108@users.noreply.github.com>
2020-12-09 19:12:05 +0600

    new data

--- Miscellaneous/innOcity Dataset/sql/Reservation_service.sql
+++
@@ -1,8 +0,0 @@
-create table Reservation_service
-( reservationID INT,
-  serviceID INT,
-  quantity INT,
-  primary key(reservationID, serviceID),
-  foreign key(reservationID) references reservation(reservationID),
-  foreign key(serviceID) references service(serviceID)
-);
